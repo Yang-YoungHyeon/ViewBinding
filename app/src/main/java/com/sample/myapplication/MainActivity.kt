d223827e17f775797b2dc1a8be772cc1d012f747
yyh03 <dev_yyh0312@norma.co.kr>
2020-03-17 16:37:45 +0900

    viewBinding

--- app/src/main/java/com/sample/myapplication/MainActivity.kt
+++ app/src/main/java/com/sample/myapplication/MainActivity.kt
@@ -2,11 +2,17 @@ package com.sample.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+/** Binding class name automatically generated for xml name */
+import com.sample.myapplication.databinding.ActivityMainBinding //viewBinding
 
 class MainActivity : AppCompatActivity() {
-
+    //Binding
+    private lateinit var viewBinding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewBinding.tvTitle.setText("This is viewBinding...")
     }
 }
